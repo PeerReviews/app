e49eecfbe3cac5ea42b06f0606de165424119b30
Louane <lou.lesur@orange.fr>
2025-02-03 17:05:13 +0100

    début template création référentiel

--- moodle/mod/studentqcm/lang/en/studentqcm.php
+++ moodle/mod/studentqcm/lang/en/studentqcm.php
@@ -5,13 +5,16 @@ $string['pluginname'] = 'Student QCM';
 $string['modulename'] = 'Student QCM';
 $string['modulenameplural'] = 'Student QCMs';
 $string['modulename_help'] = 'Cet outil permet aux étudiants de créer leurs propres QCM et aux enseignants de les évaluer.';
-$string['name'] = 'Name';
+$string['name'] = 'Nom';
+$string['surname'] = 'Prénom';
+$string['mail'] = 'Adresse mail';
 $string['description'] = 'Description';
 $string['intro'] = 'Introduction';
 $string['pluginname'] = 'Student QCM';
 $string['pluginadministration'] = 'Administration du module Student QCM';
 
 // Ajouter les chaînes pour les nouveaux champs de date
+$string['phases_title'] = 'Choix dates des phases';
 $string['start_date_1'] = 'Phase 1 : date début';
 $string['end_date_1'] = 'Phase 1 : date fin';
 $string['end_date_tt_1'] = 'Phase 1 : date fin tiers-temps';
@@ -33,6 +36,7 @@ $string['phase2_title'] = 'Évaluation de QCMs';
 $string['phase3_title'] = 'Modification des QCMs';
 
 $string['back'] = 'Retour';
+$string['validate'] = 'Valider';
 
 $string['qcm_list'] = 'Ma liste de QCMs';
 $string['add_qcm'] = 'Ajouter un QCM';
@@ -45,3 +49,50 @@ $string['answer'] = 'Réponse';
 $string['explanation'] = 'Explication';
 $string['correct_answer'] = 'Réponse vraie';
 $string['submit'] = 'Envoyer le QCM';
+
+// Creation réferentiel de compétences
+
+$string['info_referentiel_title'] = 'Informations principales du référentiel:';
+$string['name_referentiel'] = 'Choix nom du réferentiel';
+$string['date_start_referentiel'] = 'Date début réferentiel';
+$string['date_end_referentiel'] = 'Date fin réferentiel';
+
+$string['competences_title'] = 'Compétences:';
+$string['add_competences'] = 'Ajouter une compétence';
+$string['info_competence'] = 'Information sur la compétence';
+$string['name_competence'] = 'Nom de la compétence';
+
+$string['subcompetences_title'] = 'Sous-compétences:';
+$string['add_subcompetences'] = 'Ajouter une sous-compétence';
+$string['info_subcompetence'] = 'Information sur la sous-compétence';
+$string['name_subcompetence'] = 'Nom de la sous-compétence';
+$string['competence_associated'] = 'Compétence associée';
+
+$string['keyword_title'] = 'Mot-clefs:';
+$string['add_keyword'] = 'Ajouter un mot-clef';
+$string['info_keyword'] = 'Information sur le mot-clef';
+$string['name_keyword'] = 'Nom du mot-cle';
+$string['subcompetence_associated'] = 'Sous-compétence associée';
+
+// Choix étudiants
+
+$string['choice_etu_title'] = 'Choix étudiants';
+$string['choice_etu_tt_title'] = 'Choix étudiants avec tiers-temps';
+$string['import_etu'] = 'Importer liste étudiants';
+$string['selected_etu'] = 'Etudiants sélectionnés:';
+$string['add_etu'] = 'Ajouter un étudiant';
+$string['info_etu'] = 'Informations sur l’étudiant:';
+
+$string['choice_prof_title'] = 'Choix professeurs';
+$string['import_prof'] = 'Importer liste professeurs';
+$string['selected_prof'] = 'Professeurs sélectionnés:';
+$string['add_prof'] = 'Ajouter un professeur';
+$string['info_prof'] = 'Informations sur le professeur:';
+
+// Choix types eval
+
+$string['type_eval_title'] = 'Choix types évaluation:';
+$string['nb_qcm'] = 'Nombre de QCM';
+$string['nb_qcu'] = 'Nombre de QCU';
+$string['nb_tcs'] = 'Nombre de TCS';
+$string['nb_pop'] = 'Nombre de POP';
--- moodle/mod/studentqcm/mod_form.php
+++ moodle/mod/studentqcm/mod_form.php
@@ -9,16 +9,65 @@ class mod_studentqcm_mod_form extends moodleform_mod {
 
         $mform = $this->_form;
 
-        // Nom de l'activité.
-        $mform->addElement('text', 'name', get_string('name', 'mod_studentqcm'), array('size' => '64'));
-        $mform->setType('name', PARAM_TEXT);
-        $mform->addRule('name', null, 'required', null, 'client');
+        // Informations principales du référentiel.
+        $mform->addElement('html', '<h2>' . get_string('info_referentiel_title', 'mod_studentqcm') . '</h2>');
+
+        $mform->addElement('text', 'name_referentiel', get_string('name_referentiel', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('name_referentiel', PARAM_TEXT);
+        $mform->addRule('name_referentiel', null, 'required', null, 'client');
         
         $mform->addElement('editor', 'intro', get_string('intro', 'mod_studentqcm'), null);
         $mform->setType('intro', PARAM_RAW);
         $mform->addRule('intro', null, 'required', null, 'client');
 
+        $mform->addElement('date_selector', 'date_start_referentiel', get_string('date_start_referentiel', 'mod_studentqcm'));
+        $mform->addRule('date_start_referentiel', null, 'required', null, 'client');
+
+        $mform->addElement('date_selector', 'date_end_referentiel', get_string('date_end_referentiel', 'mod_studentqcm'));
+        $mform->addRule('date_end_referentiel', null, 'required', null, 'client');
+
+        // Ajout compétences
+        $mform->addElement('html', '<h2>' . get_string('competences_title', 'mod_studentqcm') . '</h2>');
+        // Ajout du bouton "Ajouter competences"
+        $mform->addElement('html', '<h3>' . get_string('info_competence', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'name_competence', get_string('name_competence', 'mod_studentqcm'), null);
+        $mform->setType('name_competence', PARAM_TEXT);
+        $mform->addRule('name_competence', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
+        // Ajout sous-compétences
+
+        $mform->addElement('html', '<h2>' . get_string('subcompetences_title', 'mod_studentqcm') . '</h2>');
+        // Ajout du bouton "Ajouter sous-competences"
+        $mform->addElement('html', '<h3>' . get_string('info_subcompetence', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'name_subcompetence', get_string('name_subcompetence', 'mod_studentqcm'), null);
+        $mform->setType('name_subcompetence', PARAM_TEXT);
+        $mform->addRule('name_subcompetence', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'competence_associated', get_string('competence_associated', 'mod_studentqcm'), null);
+        $mform->setType('competence_associated', PARAM_TEXT);
+        $mform->addRule('competence_associated', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
+        // Ajout mot-clefs
+
+        $mform->addElement('html', '<h2>' . get_string('Choix dates des phases:', 'mod_studentqcm') . '</h2>');
+        // Ajout du bouton "Ajouter mot-clefs"
+        $mform->addElement('html', '<h3>' . get_string('info_keyword', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'name_keyword', get_string('name_keyword', 'mod_studentqcm'), null);
+        $mform->setType('name_keyword', PARAM_TEXT);
+        $mform->addRule('name_keyword', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'subcompetence_associated', get_string('subcompetence_associated', 'mod_studentqcm'), null);
+        $mform->setType('subcompetence_associated', PARAM_TEXT);
+        $mform->addRule('subcompetence_associated', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
         // Ajout des champs de date.
+        $mform->addElement('html', '<h2>' . get_string('keyword_title', 'mod_studentqcm') . '</h2>');
 
         $mform->addElement('date_selector', 'start_date_1', get_string('start_date_1', 'mod_studentqcm'));
         $mform->addRule('start_date_1', null, 'required', null, 'client');
@@ -46,6 +95,89 @@ class mod_studentqcm_mod_form extends moodleform_mod {
         
         $mform->addElement('date_selector', 'end_date_tt_3', get_string('end_date_tt_3', 'mod_studentqcm'));
         $mform->addRule('end_date_tt_3', null, 'required', null, 'client');
+
+        // Choix étudiants
+
+        $mform->addElement('html', '<h2>' . get_string('choice_etu_title', 'mod_studentqcm') . '</h2>');
+
+        $mform->addElement('static', 'import_etu', get_string('import_etu', 'mod_studentqcm'), null);
+
+        $mform->addElement('static', 'selected_etu', get_string('selected_etu', 'mod_studentqcm'), null);
+        // Ajout du bouton "Ajouter un étudiant"
+        $mform->addElement('html', '<h3>' . get_string('info_etu', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'surname', get_string('surname', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('surname', PARAM_TEXT);
+        $mform->addRule('surname', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'name', get_string('name', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'mail', get_string('mail', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('mail', PARAM_TEXT);
+        $mform->addRule('mail', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
+        // Choix étudiants tiers-temps
+
+        $mform->addElement('html', '<h2>' . get_string('choice_etu_tt_title', 'mod_studentqcm') . '</h2>');
+
+        $mform->addElement('static', 'import_etu', get_string('import_etu', 'mod_studentqcm'), null);
+
+        $mform->addElement('static', 'selected_etu', get_string('selected_etu', 'mod_studentqcm'), null);
+        // Ajout du bouton "Ajouter un étudiant"
+        $mform->addElement('html', '<h3>' . get_string('info_etu', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'surname', get_string('surname', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('surname', PARAM_TEXT);
+        $mform->addRule('surname', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'name', get_string('name', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'mail', get_string('mail', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('mail', PARAM_TEXT);
+        $mform->addRule('mail', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
+        // Choix prof
+
+        $mform->addElement('html', '<h2>' . get_string('choice_prof_title', 'mod_studentqcm') . '</h2>');
+
+        $mform->addElement('static', 'import_prof', get_string('import_prof', 'mod_studentqcm'), null);
+
+        $mform->addElement('static', 'selected_prof', get_string('selected_prof', 'mod_studentqcm'), null);
+        // Ajout du bouton "Ajouter un étudiant"
+        $mform->addElement('html', '<h3>' . get_string('info_prof', 'mod_studentqcm') . '</h3>');
+
+        $mform->addElement('text', 'surname', get_string('surname', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('surname', PARAM_TEXT);
+        $mform->addRule('surname', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'name', get_string('name', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('name', PARAM_TEXT);
+        $mform->addRule('name', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'mail', get_string('mail', 'mod_studentqcm'), array('size' => '64'));
+        $mform->setType('mail', PARAM_TEXT);
+        $mform->addRule('mail', null, 'required', null, 'client');
+        //Ajout bouton "Valider"
+
+        // Choix types éval
+
+        $options = ['1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5', '6' => '6', '7' => '7', '8' => '8', '9' => '9', '10' => '10', '11' => '11', '12' => '12', '13' => '13', '14' => '14', '15' => '15', '16' => '16', '17' => '17'];
+
+        $mform->addElement('html', '<h2>' . get_string('type_eval_title', 'mod_studentqcm') . '</h2>');
+
+        $mform->addElement('select', 'choix', get_string('nb_qcm', 'mod_studentqcm'), $options);
+
+        $mform->addElement('select', 'choix', get_string('nb_qcu', 'mod_studentqcm'), $options);
+        
+        $mform->addElement('select', 'choix', get_string('nb_tcs', 'mod_studentqcm'), $options);
+        
+        $mform->addElement('select', 'choix', get_string('nb_pop', 'mod_studentqcm'), $options);
         
         $this->standard_coursemodule_elements();
 
