cf50a8f986d48359f13433f5822907624f4ea8cf
Laure-Anne Bluteau <laure-anne.bluteau@etu.univ-grenoble-alpes.fr>
2025-02-23 18:39:23 +0100

    Affichage compteurs sur la liste des productions à évaluer (Prof)

--- moodle/mod/studentqcm/teacher_production_list.php
+++ moodle/mod/studentqcm/teacher_production_list.php
@@ -48,6 +48,40 @@ if ($assigned_students) {
     foreach ($assigned_students as $student) {
         
         $prod_id = $student;
+
+        // Charger les questions de la production assignée
+        $qcms = array();
+
+        // Vérifier si l'ID de la production assignée est valide
+        if (!empty($prod_id)) {
+            // Charger les questions associées à cette production spécifique
+            $questions = $DB->get_records('studentqcm_question', array('userid' => $prod_id));
+
+            // Ajouter chaque question au tableau $qcms
+            foreach ($questions as $question) {
+                $qcms[] = $question;
+            }
+        }
+
+        $nb_eval_questions = count(array_filter($qcms, function($q) {
+            return $q->grade !== null;
+        }));
+
+        $nb_evaluated_revisions = 0;
+        $nb_total_revisions = 0;
+
+        foreach ($qcms as $qcm) {
+            $evaluations = $DB->get_records('studentqcm_evaluation', array('question_id' => $qcm->id));
+            $nb_total_revisions += count($evaluations);
+
+            foreach ($evaluations as $evaluation) {
+                if ($evaluation->grade != null ) {
+                    $nb_evaluated_revisions++;
+                }
+            }
+        }
+
+
         echo "<div class='p-4 bg-white rounded-3xl shadow flex items-center justify-between'>";
             echo "<p class='font-semibold text-2xl text-gray-700 flex items-center gap-2'>";
             echo "<i class='fas fa-clipboard text-indigo-400 mr-2'></i>";
@@ -55,11 +89,15 @@ if ($assigned_students) {
             echo "</p>";
 
             // Bouton d'évaluation
-            echo "<div>";
-            echo "<a href='teacher_production_eval.php?id={$id}&prod_id={$prod_id}' class='px-4 py-2 bg-indigo-400 text-white text-lg font-semibold rounded-2xl hover:bg-indigo-500'>";
-            echo "<i class='fas fa-pen-to-square mr-2'></i> " . get_string('evaluate', 'mod_studentqcm');
-            echo "</a>";
+            echo "<div class='flex items-center text-xl text-gray-700'>";
+                echo "<p class='mr-8 font-medium text-lg'>" . get_string('nb_evaluated_question', 'mod_studentqcm') . " : <span id='nb-eval-questions'>" . $nb_eval_questions . " / " . count($qcms) . "</span></p>";
+                echo "<p class='mr-8 font-medium text-lg'>" . get_string('nb_evaluated_revision', 'mod_studentqcm') . " : <span id='nb-eval-revisions'>" . $nb_evaluated_revisions . " / " . $nb_total_revisions . "</span></p>";
+
+                echo "<a href='teacher_production_eval.php?id={$id}&prod_id={$prod_id}' class='px-4 py-2 bg-indigo-400 text-white text-lg font-semibold rounded-2xl hover:bg-indigo-500'>";
+                echo "<i class='fas fa-pen-to-square mr-2'></i> " . get_string('evaluate', 'mod_studentqcm');
+                echo "</a>";
             echo "</div>";
+
         echo "</div>";
         
     }
